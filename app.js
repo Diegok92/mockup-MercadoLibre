41c77c196ce92900c9a5121c5e39065faefb7fe7
Diego Kleiman <diego.kleiman@gmail.com>
2023-03-25 15:22:20 -0300

    1er commit - 1er avance html- css y mediaqueries

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000, function () {
+  console.log("serivdor corriendo en 3000");
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
